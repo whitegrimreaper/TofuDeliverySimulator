dc2532ecf2b256770792330c820821b213a1f94d
whitegrimreaper <theDumb12@yahoo.com>
2018-04-28 14:09:27 -0700

    Moved car script to new Scripts folder, deleted some extra assets

---
+++ TofuDeliverySimulator/Assets/Scripts/CarScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(rb.velocity.magnitude <= 20.0f)
+        {
+            rb.AddForce(new Vector3(1.5f, 0, 0));
+        }
+	}
+}
